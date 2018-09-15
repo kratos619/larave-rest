605648cda1ec421fa7800a6e46f965892c9cebd4
kratos619 <gaurav.palaspagar@hotmail.com>
2018-09-15 16:42:10 +0530

    Creting routes and controller

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::group(['prefix' => 'api/v1'], function () {
+
+// Route::resource('meeting', 'MeetingController',[
+//     'except' => ['edit','create']
+// ]);
+
+
+// Route::resource('meeting/registration', 'RegistrationController',[
+//     'only' => ['store','destroy']
+// ]);
+
+
+// Route::post('user', [
+//     'uses' => 'AuthController@store' 
+// ]);
+
+
+// Route::post('user/signin', [
+//     'uses' => 'AuthController@signin' 
+// ]);
+
+    
+// });
